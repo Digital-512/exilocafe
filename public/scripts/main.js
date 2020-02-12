8dd0e03992a079e384fbe3cccb468b975a7c398d
Digital-512 <57298727+Digital-512@users.noreply.github.com>
2020-02-12 21:00:11 +0200

    Added dropdown menu
    ...and small changes in stylesheet.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,44 @@
 "use strict";
 
-var mdl_nav_links = $('.mdl-navigation__link');
+// Navigation
+let mdl_nav_links = $('.mdl-navigation__link');
 mdl_nav_links.each(function (i, el) {
-    var data_location = $(el).data('location');
+    let data_location = $(el).data('location');
     if (data_location) {
         $(el).click(function () {
             location.href = '/' + data_location;
         });
+    }
+});
+
+// Close the dropdown if the user clicks outside of it
+$(window).click(function (event) {
+    let dropdowns = $('.dropdown-content');
+    dropdowns.each(function (i, el) {
+        if ($(el).css('display') !== 'none' && !$(el).closest('.has-dropdown').find($(event.target)).length) {
+            $(el).hide();
+        }
+    });
+});
+
+// Find all buttons that have dropdown
+let dropdown_buttons = $('.has-dropdown');
+dropdown_buttons.click(function (event) {
+    // Show or hide dropdown menu
+    let content = $(this).find('.dropdown-content');
+    if (content.css('display') === 'none') {
+        content.show();
+    } else if ($(event.target).closest('.dropdown-content').length) {
+        // Prevent closing if no-close is set
+        if ($(event.target).closest('.dropdown-no-close').length) {
+            return;
+        }
+        // Wait some time before closing menu, so the user can see the effect
+        setTimeout(function () {
+            content.hide();
+        }, 50);
+    } else {
+        // If button is clicked again, close immediately
+        content.hide();
     }
 });
