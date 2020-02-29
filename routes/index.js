fd07ffda5e4fb570462b0d843d893df23b1329aa
Digital-512 <57298727+Digital-512@users.noreply.github.com>
2020-02-29 19:47:03 +0200

    Build without unnecessary Material modules
    And added dev packages.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = function (config, lang, database, auth) {
-    router.get('/:module?', function (req, res) {
+    router.get('/:module?', async function (req, res) {
         if (req.params.module && config.installedModules.hasOwnProperty(req.params.module)) {
             res.render('index', { config: config, lang: lang, database: database, module: req.params.module });
         } else {
