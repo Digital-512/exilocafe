88cf08258d0b7a3c37a0a1dbd643b10f579b33e3
Digital-512 <57298727+Digital-512@users.noreply.github.com>
2020-03-22 20:39:36 +0200

    Updated dependencies and removed jQuery

---
+++ public/scripts/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+window.addEventListener('click', function (event) {
+    // Close the dropdown if the user clicks outside of it.
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    for (var i = 0; i < dropdowns.length; i++) {
+        var el = dropdowns[i];
+        if (el.style.display !== 'none' && !el.closest('.has-dropdown').contains(event.target)) {
+            el.style.display = 'none';
+        }
+    }
+
+    // Find all buttons that have dropdown.
+    var dropdownButton = event.target.closest('.has-dropdown');
+    if (dropdownButton) {
+        var content = dropdownButton.querySelector('.dropdown-content');
+        if (!content.style.display || content.style.display === 'none') {
+            content.style.display = 'block';
+        } else if (event.target.closest('.dropdown-content').length) {
+            // Prevent closing if no-close is set.
+            if (event.target.closest('.dropdown-no-close').length) {
+                return;
+            }
+            // Wait some time before closing menu, so the user can see the effect.
+            setTimeout(function () {
+                content.style.display = 'none';
+            }, 50);
+        } else {
+            // If button is clicked again, close immediately.
+            content.style.display = 'none';
+        }
+    }
+});
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,13 @@ const router = express.Router();
 module.exports = function (config, lang, database, auth) {
     router.get('/:module?', async function (req, res) {
         if (req.params.module && config.installedModules.hasOwnProperty(req.params.module)) {
-            res.render('index', { config: config, lang: lang, database: database, module: req.params.module });
+            let globalData = {
+                config,
+                lang,
+                database,
+                theme: 'default'
+            }
+            res.render('index', { globalData: globalData, module: req.params.module });
         } else {
             res.redirect('/' + config.general.defaultModule);
         }
